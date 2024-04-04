67a3ffeb9a9d50574e6f9816335e88a57c3d22b0
viet-quanggg <nguyenvietquang099@gmail.com>
2024-04-04 11:29:39 +0700

    update functions

--- Repository/Repository/SessionRepository.cs
+++ Repository/Repository/SessionRepository.cs
@@ -9,7 +9,6 @@ public class SessionRepository : ISessionRepository
     public Task<List<Session>> GetSessionsToday() => SessionManagement.Instance.GetSessionsToday();
 
     public Task<List<Session>> GetSessionsByAppointmentDate(DateTime selectedDate) => SessionManagement.Instance.GetSessionsByAppointmentDate(selectedDate);
-
     public Task<List<Session>> GetSessionsBySessionDate(DateTime selectedDate) =>
         SessionManagement.Instance.GetSessionsBySessionDate(selectedDate);
 }
